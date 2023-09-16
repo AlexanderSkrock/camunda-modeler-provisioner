41f3e13236802a3d2e263a493e8508d880898551
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-16 20:06:46 +0200

    Added command to list available versions

--- bin/camunda-modeler.js
+++ bin/camunda-modeler.js
@@ -1,9 +1,11 @@
-const { install } = require('../lib');
+const { install, getVersions } = require('../lib');
 
 module.exports = function (args, stdout, stderr) {
     switch(args[0]) {
+        case 'versions':
+            return getVersions().then((versions) => stdout.write(`Versions:\n${versions.join("\n")}\n`))
         case 'install': {
-            return install(args[1]).then(() => stdout.write('Installation succeeded'));
+            return install(args[1]).then((res) => stdout.write(res + '\n'));
         }
         default: stderr.write(`Unknown parameters: ${args}\n`);
     }
