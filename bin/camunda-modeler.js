94eda9d2a28be93986e963110c171e1139e748d5
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-17 20:17:24 +0200

    Added separate download sub command

--- bin/camunda-modeler.js
+++ bin/camunda-modeler.js
@@ -5,8 +5,11 @@ module.exports = function (args, stdout, stderr) {
     switch (args[0]) {
     case 'versions':
         return getAllVersions().then((versions) => stdout.write(`Versions:\n${versions.join('\n')}\n`));
+    case 'download': {
+        return download(getCurrentOS(), args[1]).then((res) => stdout.write(`Download was finished successful: ${res}`));
+    }
     case 'install': {
-        return install(getCurrentOS(), args[1]).then((res) => stdout.write(`Result: ${res}\n`));
+        return install(getCurrentOS(), args[1]).then((res) => stdout.write(`Installation was finished successful: ${res}\n`));
     }
     default: return Promise.reject(new Error(`Unknown parameters: ${args}`));
     }
