8b52a00e3198528ac5f1a741a0af5b00aa8a2860
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-18 08:19:30 +0200

    Refactor to use yargs instead of custom code

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
-require('./bootstrap')(
-    require('./camunda-modeler'),
-    require('./help'),
-    require('./version'),
-);
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const { getCurrentOS } = require('./process');
+const { getAllVersions, download, install } = require('../lib');
+
+// eslint-disable-next-line
+yargs(hideBin(process.argv))
+    .scriptName('camunda-modeler')
+    .command(
+        'versions',
+        'List available versions of the Camunda Modeler',
+        () => {},
+        () => getAllVersions().then((versions) => process.stdout.write(`Versions:\n${versions.join('\n')}\n`)),
+    )
+    .command(
+        'download',
+        'Donwload Camunda Modler',
+        (yargs) => yargs.version(false).option('version'),
+        (args) => download(getCurrentOS(), args.version).then((res) => process.stdout.write(`Download was finished successful: ${res}`)),
+    )
+    .command(
+        'install',
+        'Install Camunda Modeler',
+        (yargs) => yargs.version(false).option('version'),
+        (args) => install(getCurrentOS(), args.version).then((res) => process.stdout.write(`Installation was finished successful: ${res}\n`)),
+    )
+    .argv;
