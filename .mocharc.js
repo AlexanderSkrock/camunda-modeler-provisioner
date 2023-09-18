d226d8fb529a05973e690b7e6ff2532dfa71bd1a
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-18 09:23:51 +0200

    Added first test case based on mocha

---
+++ .mocharc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// This is a JavaScript-based config file containing every Mocha option plus others.
+// If you need conditional logic, you might want to use this type of config,
+// e.g. set options via environment variables 'process.env'.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  spec: ['test/**/*.spec.js'],
+  'watch-files': ['lib/**/*.js', 'test/**/*.js'],
+  'watch-ignore': ['bin/**']
+};
---
+++ test/versions/versions.spec.js
@@ -0,0 +1,57 @@
+const assert = require('node:assert');
+const { mock } = require('node:test');
+const { after, before, describe, it } = require('mocha');
+const { getAllVersions } = require('../../lib/versions');
+
+describe('Versions', function () {
+    describe('#getAllVersions', function () {
+        before(() => {
+            mock.method(global, 'fetch', () => Promise.resolve({
+                status: 200,
+                headers: { 'Content-type': 'application/json' },
+                json: () => {
+                    const js = require('./versions_response.json');
+                    console.log(js);
+                    return js;
+                },
+            }),
+            );
+        });
+        it('should return array of version tags', async function () {
+            const versions = await getAllVersions();
+            assert.deepEqual(versions, [
+                'v5.15.0',
+                'v5.14.0',
+                'v5.13.0',
+                'v5.12.1',
+                'v5.12.0',
+                'v5.11.0',
+                'v5.10.0',
+                'v5.9.0',
+                'v5.8.0',
+                'v5.7.0',
+                'v5.6.0',
+                'v5.5.1',
+                'v5.5.0',
+                'v5.4.2',
+                'v5.4.1',
+                'v5.4.0',
+                'v5.3.0',
+                'v5.2.0',
+                'v5.1.0',
+                'v5.0.0',
+                'v5.0.0-rc.0',
+                'v5.0.0-alpha.1',
+                'v5.0.0-alpha.0',
+                'v4.12.0',
+                'v4.12.0-rc.1',
+                'v4.11.1',
+                'v4.11.0',
+                'v4.11.0-rc.1',
+                'v4.11.0-rc.0',
+                'v4.10.0',
+            ]);
+        });
+        after(() => mock.reset());
+    });
+});
