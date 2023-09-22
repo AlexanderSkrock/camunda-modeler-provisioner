c8a447f64177f40e1493efb861e8a2f975c2bbf3
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-22 10:04:14 +0200

    Added configuration option to the CLI interface

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,8 @@
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-const { join } = require('node:path');
-
 const { getAllVersions, download, install, launch } = require('../lib');
+const { getConfig } = require('../lib');
 
 // eslint-disable-next-line
 yargs(hideBin(process.argv))
@@ -20,8 +19,9 @@ yargs(hideBin(process.argv))
         'Donwload Camunda Modeler',
         (yargs) => yargs
             .version(false)
-            .option('version')
-            .option('path', { default: join(process.cwd(), '.camunda-modeler') }),
+            .option('version', { default: getConfig().getVersion() })
+            .option('version', { default: getConfig().getVersion() })
+            .option('cache-path', { default: getConfig().getCachePath() }),
         (args) => download(process.platform, args.version, args.path).then((res) => process.stdout.write(`Download was finished successfully: ${res}`)),
     )
     .command(
@@ -29,15 +29,16 @@ yargs(hideBin(process.argv))
         'Install Camunda Modeler',
         (yargs) => yargs
             .version(false)
-            .option('version')
-            .option('path', { default: join(process.cwd(), '.camunda-modeler') })
+            .option('version', { default: getConfig().getVersion() })
+            .option('path', { default: getConfig().getInstallationPath() })
+            .option('cache-path', { default: getConfig().getCachePath() })
             .option('link-plugin'),
-        (args) => install(process.platform, args.version, args.path, args['link-plugin'] && [args['link-plugin']]).then((res) => process.stdout.write(`Installation was finished successfully: ${res}\n`)),
+        (args) => install(process.platform, args.version, args.path, args['cache-path'], args['link-plugin'] && [args['link-plugin']]).then((res) => process.stdout.write(`Installation was finished successfully: ${res}\n`)),
     )
     .command(
         'launch',
         'Launch Camunda Modeler',
-        (yargs) => yargs.option('path', { default: join(process.cwd(), '.camunda-modeler') }),
+        (yargs) => yargs.option('path', { default: getConfig().getInstallationPath() }),
         args => launch(process.platform, args.path).catch(err => `An error occurred: ${err}`),
     )
     .argv;
