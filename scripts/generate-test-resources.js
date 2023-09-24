65c3de55882095d559d268d4e8d16db35cf0340f
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-24 21:13:01 +0200

    Added automatic download for all releases

--- scripts/generate-test-resources.js
+++ scripts/generate-test-resources.js
@@ -14,16 +14,28 @@ try {
 const responseMapping = {};
 
 const releasesUrl = 'https://api.github.com/repos/camunda/camunda-modeler/releases';
+const latestReleaseUrl = `${releasesUrl}/latest`;
+const taggedReleaseUrl = tag => `${releasesUrl}/tags/${tag}`;
 
 Promise.all([
-    fetch(releasesUrl).then(response => response.json()).then(json => {
+    fetch(releasesUrl).then(response => response.json()).then(releases => {
         const fileName = crypto.webcrypto.randomUUID() + '.json';
         responseMapping[releasesUrl] = fileName;
+        return Promise.all([
+            writeFile(join(testResourcesPath, fileName), JSON.stringify(releases)),
+            ...releases.map(r => fetch(taggedReleaseUrl(r.name)).then(response => response.json()).then(release => {
+                const fileName = crypto.webcrypto.randomUUID() + '.json';
+                responseMapping[taggedReleaseUrl(r.name)] = fileName;
+                return writeFile(join(testResourcesPath, fileName), JSON.stringify(release));
+            }))
+        ]);
+    }),
+    fetch(latestReleaseUrl).then(response => response.json()).then(json => {
+        const fileName = crypto.webcrypto.randomUUID() + '.json';
+        responseMapping[latestReleaseUrl] = fileName;
         return writeFile(join(testResourcesPath, fileName), JSON.stringify(json));
     })
 ]).then(() => writeFile(join(testResourcesPath, "response_mappings.json"), JSON.stringify(responseMapping)));
-// const latestReleaseUrl = `${releasesUrl}/latest`;
-// const releaseTagsUrl = `${releasesUrl}/tags/${versionTag}`;
 
 // release.assets[x].browser_download_url
 
