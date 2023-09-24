f5cf53c29e0fb007b32671cbe60a87b4ea011a24
Alexander Skrock <alexanderskrock@gmx.de>
2023-09-24 19:20:00 +0200

    First steps to retrieving request data

---
+++ scripts/generate-test-resources.js
@@ -0,0 +1,29 @@
+const { accessSync, constants, rm, mkdirSync, writeFileSync } = require('node:fs');
+const { writeFile } = require('node:fs/promises');
+const { join } = require('node:path');
+
+const testResourcesPath = join(process.cwd(), 'test-resources');
+
+try {
+    accessSync(testResourcesPath, constants.W_OK | constants.R_OK)
+} catch (error) {
+    mkdirSync(testResourcesPath);
+}
+
+const responseMapping = {};
+
+const releasesUrl = 'https://api.github.com/repos/camunda/camunda-modeler/releases';
+
+Promise.all([
+    fetch(releasesUrl).then(response => response.json()).then(json => {
+        const filePath = join(testResourcesPath, 'random' + '.json');
+        responseMapping[releasesUrl] = filePath;
+        return writeFile(filePath, JSON.stringify(json));
+    })
+]).then(() => writeFile(join(testResourcesPath, "response_mappings.json"), JSON.stringify(responseMapping)));
+// const latestReleaseUrl = `${releasesUrl}/latest`;
+// const releaseTagsUrl = `${releasesUrl}/tags/${versionTag}`;
+
+// release.assets[x].browser_download_url
+
+// Assets
